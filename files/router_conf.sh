90b8184c307d3f547e177cae56c1764e05efab87
gsergey99 <Sergio.Jimenezl@alu.uclm.es>
2020-12-23 20:54:23 +0100

    Only once done. Pending Web Server and TFTP

--- files/router_conf.sh
+++ files/router_conf.sh
@@ -2,6 +2,10 @@
 
 
 # Configuracion para el dnsmasq.conf
+
+var_init=$(sudo tail -n1 /etc/dnsmasq.conf)
+
+if [ $var_init != '#end' ];then  
 sudo bash -c 'echo "port=53" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "domain-needed" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "bogus-priv" >> /etc/dnsmasq.conf'
@@ -10,14 +14,12 @@ sudo bash -c 'echo "interface=eth3" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "listen-address=192.168.0.1" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "expand-hosts" >> /etc/dnsmasq.conf'
 sudo bash -c 'echo "domain=example.com" >> /etc/dnsmasq.conf'
-
 sudo bash -c 'echo "dhcp-range=eth3,192.168.0.2,192.168.0.254,255.255.255.0,24h" >> /etc/dnsmasq.conf'
+sudo bash -c 'echo "#end" >> /etc/dnsmasq.conf'
 
-# Configuración para el resolv.conf
 sudo bash -c '> /etc/resolv.conf'
 sudo bash -c 'echo "nameserver 10.35.0.1" >> /etc/resolv.conf'
 sudo bash -c 'echo "nameserver 8.8.8.8" >> /etc/resolv.conf'
 
-
 sudo systemctl restart dnsmasq.service
-# sudo systemctl status dnsmasq.service
+fi
--- files/web_server_conf.sh
+++ files/web_server_conf.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+var_init=$(sudo tail -n1 /etc/default/tftpd-hpa)
+
+if [ $var_init != '#end' ];then 
+
 sudo bash -c '> /etc/default/tftpd-hpa'
 
 sudo bash -c 'echo "## /etc/default/tftpd-hpa" >> /etc/default/tftpd-hpa'
@@ -7,6 +11,12 @@ sudo bash -c 'echo "TFTP_USERNAME="tftp"" >> /etc/default/tftpd-hpa'
 sudo bash -c 'echo "TFTP_DIRECTORY="/tftp"" >> /etc/default/tftpd-hpa'
 sudo bash -c 'echo "TFTP_ADDRESS="0.0.0.0:69"" >> /etc/default/tftpd-hpa'
 sudo bash -c 'echo "TFTP_OPTIONS="--secure --create"" >> /etc/default/tftpd-hpa'
+
+sudo bash -c 'echo "#end" >> /etc/default/tftpd-hpa'
+
+sudo systemctl restart tftpd-hpa.service
+fi
+
 
 
 
